65e0d2feb1727f458c08686e31314422d0519cd1
Vijay Patel <veejspeej@gmail.com>
2018-09-01 20:35:58 +0100

    Corrects data directory. Creates a topic as part of cluster script.

--- start-kafka-cluster.sh
+++ start-kafka-cluster.sh
@@ -16,6 +16,14 @@ sudo chown $USER /var/lib/zookeeper
 sudo chown $USER /var/lib/kafka
 
 # Start kafka cluster and zookeeper node
-./kafka_2.11-2.0.0/bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
-./kafka_2.11-2.0.0/bin/kafka-server-start.sh -daemon config/server-1.properties
-./kafka_2.11-2.0.0/bin/kafka-server-start.sh -daemon config/server-2.properties 
+./kafka/bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
+./kafka/bin/kafka-server-start.sh -daemon config/server-1.properties
+./kafka/bin/kafka-server-start.sh -daemon config/server-2.properties 
+
+# Create topics - 2 partitions and 1 replica for the simple setup of two local nodes
+./kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic twitter-data
+
+# Describe topic setup
+./kafka/bin/kafka-topics.sh --list --zookeeper localhost:2181
+./kafka/bin/kafka-topics.sh --describe --zookeeper localhost:2181 --topic twitter-data
+
