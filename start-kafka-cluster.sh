a6e0f1b5682808a5d06a9c83cfa5f6c8f63ae121
Vijay Patel <veejspeej@gmail.com>
2018-09-04 09:25:10 +0100

    Wait for 5 seconds before attempting to create/describe the topic as it takes a short while for kafka cluster to start.

--- start-kafka-cluster.sh
+++ start-kafka-cluster.sh
@@ -20,6 +20,8 @@ sudo chown $USER /var/lib/kafka
 ./kafka/bin/kafka-server-start.sh -daemon config/server-1.properties
 ./kafka/bin/kafka-server-start.sh -daemon config/server-2.properties 
 
+sleep 5
+
 # Create topics - 2 partitions and 1 replica for the simple setup of two local nodes
 ./kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic twitter-data
 
