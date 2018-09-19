664021f4b64cafe47368347ca9813557a91fd85c
Vijay Patel <veejspeej@gmail.com>
2018-09-19 14:02:31 +0100

    Extract install of kafka from cluster start.

--- kafka-cluster-start.sh
+++ kafka-cluster-start.sh
@@ -1,20 +1,3 @@
-# Download and install kafka
-KAFKA_VERSION="kafka_2.11-2.0.0"
-
-wget -nc http://apache.mirror.anlx.net/kafka/2.0.0/${KAFKA_VERSION}.tgz
-
-if [ ! -d "./${KAFKA_VERSION}" ]
-then
-    tar -xvf ${KAFKA_VERSION}.tgz
-    ln -s ${KAFKA_VERSION} kafka
-fi
-
-# Make data directories
-sudo mkdir /var/lib/zookeeper
-sudo mkdir /var/lib/kafka
-sudo chown $USER /var/lib/zookeeper
-sudo chown $USER /var/lib/kafka
-
 # Start kafka cluster and zookeeper node
 ./kafka/bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
 ./kafka/bin/kafka-server-start.sh -daemon config/server-1.properties
---
+++ kafka-install.sh
@@ -0,0 +1,16 @@
+# Download and install kafka
+KAFKA_VERSION="kafka_2.11-2.0.0"
+
+wget -nc http://apache.mirror.anlx.net/kafka/2.0.0/${KAFKA_VERSION}.tgz
+
+if [ ! -d "./${KAFKA_VERSION}" ]
+then
+    tar -xvf ${KAFKA_VERSION}.tgz
+    ln -s ${KAFKA_VERSION} kafka
+fi
+
+# Make data directories
+sudo mkdir /var/lib/zookeeper
+sudo mkdir /var/lib/kafka
+sudo chown $USER /var/lib/zookeeper
+sudo chown $USER /var/lib/kafka
