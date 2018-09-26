c082d9517a4153377ea66b3f247f3f6e59347f5f
Vijay Patel <veejspeej@gmail.com>
2018-09-26 23:18:03 +0100

    Extracts away install of spark. Adds pyspark to pip dependencies for defining python spark jobs.

--- pyspark-start.sh
+++ pyspark-start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sh install-py-deps.sh
-sh install-spark.sh
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 source activate py36-twitter-mining
 ./spark-2.3.1-bin-hadoop2.7/bin/pyspark
