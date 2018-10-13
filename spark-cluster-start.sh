4570ea69defe11f13aae1eb08db56d8e8f8d89fb
Vijay Patel <veejspeej@gmail.com>
2018-10-13 18:55:19 +0100

    Start spark on localhost

--- spark-cluster-start.sh
+++ spark-cluster-start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-./spark/sbin/start-master.sh --webui-port 8080 --port 7077 --host `hostname`
-./spark/sbin/start-slave.sh spark://`hostname`:7077
+./spark/sbin/start-master.sh --webui-port 8080 --port 7077 --host localhost
+./spark/sbin/start-slave.sh spark://localhost:7077
