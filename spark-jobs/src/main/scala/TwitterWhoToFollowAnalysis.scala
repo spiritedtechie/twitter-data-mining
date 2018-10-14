d936d454c50640c5529c60dcf9da0f1020abe089
Vijay Patel <veejspeej@gmail.com>
2018-10-14 13:03:06 +0100

    Grabs retweets from the stream and prints out those rows.
    Experimenting with the Spark SQL API.

--- spark-jobs/src/main/scala/TwitterWhoToFollowAnalysis.scala
+++ spark-jobs/src/main/scala/TwitterWhoToFollowAnalysis.scala
@@ -1,6 +1,8 @@
 import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.dstream.InputDStream
 import org.apache.spark.streaming.kafka010._
@@ -10,7 +12,8 @@ object TwitterWhoToFollowAnalysis {
   def main(args: Array[String]) {
 
     val sparkConf = new SparkConf().setAppName("TwitterWhoToFollowAnalysis")
-    val streamingContext = new StreamingContext(sparkConf, Seconds(2))
+    val sparkSession: SparkSession = SparkSession.builder.config(sparkConf).getOrCreate()
+    val streamingContext = new StreamingContext(sparkSession.sparkContext, Seconds(2))
 
     val kafkaParams = Map[String, Object](
       ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9094",
@@ -20,6 +23,8 @@ object TwitterWhoToFollowAnalysis {
       ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
       ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer]
     )
+
+    // Create stream
     val stream: InputDStream[ConsumerRecord[String, String]] =
       KafkaUtils.createDirectStream[String, String](
         streamingContext,
@@ -27,8 +32,31 @@ object TwitterWhoToFollowAnalysis {
         ConsumerStrategies.Subscribe[String, String](Set("twitter-data"), kafkaParams)
       )
 
-    val lines = stream.map(record => record.key)
-    lines.print()
+    // Process stream
+    stream.foreachRDD((rdd: RDD[ConsumerRecord[String, String]]) => {
+
+      val rdd1: RDD[String] = rdd.map(cr => cr.value())
+
+      val ds: Dataset[String] = sparkSession.createDataset(rdd1)(Encoders.STRING)
+
+      val df: DataFrame = sparkSession.read.json(ds)
+
+
+      if (df.count() >= 1) {
+
+        val dfHasRetweets = !df.schema.fields.filter(sf => "retweeted_status".equals(sf.name)).isEmpty
+
+        if (dfHasRetweets) {
+          val rowsThatAreRetweets = df.select("id", "retweeted_status")
+            .where("retweeted_status is not null")
+
+          rowsThatAreRetweets.head(5).foreach(r => println("**** row: " + r))
+
+        }
+
+      }
+
+    })
 
     streamingContext.start()
     streamingContext.awaitTermination()
